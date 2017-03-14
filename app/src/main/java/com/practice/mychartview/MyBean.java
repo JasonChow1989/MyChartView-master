09089cf0dae680c65ed9d98387de7c125e37d956
Jason <100360258@qq.com>
2017-03-14 10:41:28 +0800

    Initial commit

---
+++ app/src/main/java/com/practice/mychartview/MyBean.java
@@ -0,0 +1,27 @@
+package com.practice.mychartview;
+
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+
+public class MyBean {
+
+    private String name;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
